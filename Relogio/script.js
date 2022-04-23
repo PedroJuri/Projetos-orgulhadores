74e547e4028775ecf4e56c52bed1c6c0c130d55e
PedroJuri <p3dr1n018@outlook.com>
2022-04-22 23:11:57 -0300

    att-3-relogio-analogico

--- Relogio/script.js
+++ Relogio/script.js
@@ -17,3 +17,20 @@ function corrigirHorario(numero){
 
 atualizarTempo();
 setInterval(atualizarTempo, 1000);
+
+
+const deg = 6;
+const hr = document.querySelector(".hr");
+const mn = document.querySelector(".mn");
+const sg = document.querySelector(".sg");
+
+setInterval(() =>{
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
+
+    hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
+    mn.style.transform = `rotateZ(${mm}deg)`;
+    sg.style.transform = `rotateZ(${ss}deg)`;
+})
